a8f20fc8dc31f0f8dc51aa38d9a583e3a2757974
saman.pirooz@live.com <saman.pirooz@live.com>
2021-11-20 19:53:30 +0330

    Create Media Controller

---
+++ Swegrant.Server/Controllers/MediaController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Swegrant.Server.Controllers
+{
+    public class MediaController : ApiController
+    {
+        [HttpGet]
+        public DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
+        }
+
+    }
+}
