02599268225a8b28659c0f2aa0dc672d1c277b3f
saman.pirooz@live.com <saman.pirooz@live.com>
2022-01-30 23:22:32 +0330

    Media Controller Get Files Url List Added

--- Swegrant.Server/Controllers/MediaController.cs
+++ Swegrant.Server/Controllers/MediaController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Swegrant.Server.Controllers
 {
@@ -8,12 +10,34 @@ namespace Swegrant.Server.Controllers
     [ApiController]
     public class MediaController : ControllerBase
     {
+        //[HttpGet, Route("api/media/GetCurrentTime")]
+        //public DateTime GetCurrentTime()
+        //{
+        //    return DateTime.Now;
+        //}
+
         [HttpGet]
-        public DateTime GetCurrentTime()
+        public string[] GetFileURL()
         {
-            return DateTime.Now;
+            DirectoryInfo AudioDirectory = new DirectoryInfo($"{Directory.GetCurrentDirectory()}\\Video\\Audio");
+            //string[] files = Directory.GetFiles(AudioDirectory);
+            FileInfo[] Files = AudioDirectory.GetFiles();
+
+            string localIP = Helpers.NetworkHelpers.GetLocalIPv4();
+            string port = "5000";
+
+            List<string> urls = new List<string>();
+            foreach (FileInfo file in Files)
+            {
+                
+                urls.Add(
+                $"http://{localIP}:{port}/Video/Audio/{file.Name}"
+                );
+            }
+            return urls.ToArray();
+
+
+
         }
-
-
     }
 }
