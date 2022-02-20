f42fafa92ff57466df89ce8a950656cb0156bf1b
saman.pirooz@live.com <saman.pirooz@live.com>
2022-02-20 18:19:29 +0330

    Change Server IP

---
+++ Swegrant.Server/Helpers/AppConfigHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swegrant.Server.Helpers
+{
+    public class AppConfigHelpers
+    {
+        public static void SaveConfig(string key, object value)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+
+            settings[key].Value = value.ToString();
+
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+        }
+
+        public static object LoadConfig(string key)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+
+            return settings[key].Value;
+        }
+    }
+}
--- Swegrant.Server/MainWindow.xaml.cs
+++ Swegrant.Server/MainWindow.xaml.cs
@@ -29,9 +29,9 @@ namespace Swegrant.Server
     {
 
         public static MainWindow Singleton
-        { 
-            get; 
-            private set; 
+        {
+            get;
+            private set;
         }
 
         public Mode CurrentMode;
@@ -52,7 +52,7 @@ namespace Swegrant.Server
 
         private Task autoLine;
 
-      
+
 
         //private HttpSelfHostServer restService;
         //private IDisposable apiServer;
@@ -77,6 +77,17 @@ namespace Swegrant.Server
                 }
             }
 
+            string[] ips = Helpers.NetworkHelpers.GetAllIPs();
+            cmbServerIP.ItemsSource = ips;
+            object savedIP = Helpers.AppConfigHelpers.LoadConfig("ServerIP");
+            if (settings["ServerIP"] != null)
+            {   
+                if (ips.Contains(savedIP.ToString()))
+                {
+                    cmbServerIP.SelectedItem = savedIP;
+                }
+            }
+
             MainWindow.Singleton = this;
 
 
@@ -84,18 +95,29 @@ namespace Swegrant.Server
 
         private async void btnStartServer_Click(object sender, RoutedEventArgs e)
         {
-            string localIP = Helpers.NetworkHelpers.GetLocalIPv4();
-            string port = Swegrant.Shared.Models.ChatSettings.DefaultPort;
-            if (!string.IsNullOrEmpty(localIP))
+            try
             {
-                txtServerAddress.Text = $"{localIP}:{port}";
-                Task task = new Task(() => StartServer(localIP, port));
-                task.Start();
-                MessageBox.Show("Server Started");
+                //string localIP = Helpers.NetworkHelpers.GetLocalIPv4();
+                string localIP = cmbServerIP.SelectedItem.ToString();
+                string port = Swegrant.Shared.Models.ChatSettings.DefaultPort;
+                if (!string.IsNullOrEmpty(localIP))
+                {
+                    txtServerAddress.Text = $"{localIP}:{port}";
+                    Task task = new Task(() => StartServer(localIP, port));
+                    task.Start();
+
+                    Helpers.AppConfigHelpers.SaveConfig("ServerIP", localIP);
+
+                    MessageBox.Show("Server Started");
+                }
+                else
+                {
+                    MessageBox.Show("IP NOT FOUND!");
+                }
             }
-            else
+            catch(Exception ex)
             {
-                MessageBox.Show("IP NOT FOUND!");
+                MessageBox.Show(ex.Message);
             }
         }
 
@@ -254,394 +276,7 @@ namespace Swegrant.Server
             }
         }
 
-        //private void FillTHSbutitleListBox(string text)
-        //{
-        //    List<string> list = text.Split(new string[] { Environment.NewLine + Environment.NewLine },
-        //                      StringSplitOptions.RemoveEmptyEntries).ToList();
-        //    this.currentSub = new List<Subtitle>();
-        //    this.currentSubIndex = 0;
 
-        //    //List<string> subLine = new List<string>();
-        //    foreach (var item in list)
-        //    {
-        //        Subtitle sub = new Subtitle();
-        //        string[] parts = item.Split(new string[] { Environment.NewLine },
-        //                       StringSplitOptions.RemoveEmptyEntries).ToArray();
-        //        sub.Id = Convert.ToInt32(parts[0]);
-        //        string[] times = parts[1].Split("-->", StringSplitOptions.RemoveEmptyEntries).ToArray();
-        //        sub.StartTime = TimeSpan.Parse(times[0].Replace(',', '.').Trim());
-        //        sub.EndTime = TimeSpan.Parse(times[1].Replace(',', '.').Trim());
-        //        string line = "";
-        //        for (int i = 2; i < parts.Length; i++)
-        //        {
-        //            line += parts[i];
-        //        }
-        //        sub.Text = line;
-        //        currentSub.Add(sub);
-        //    }
-        //    this.lstthSub.ItemsSource = currentSub.Select(c => c.Text).ToArray();
-        //    this.lstthSub.SelectedIndex = 0;
-        //}
-
-        //private void FillVideoSbutitleListBox(string text)
-        //{
-        //    List<string> list = text.Split(new string[] { Environment.NewLine + Environment.NewLine },
-        //                       StringSplitOptions.RemoveEmptyEntries).ToList();
-        //    this.currentSub = new List<Subtitle>();
-
-        //    //List<string> subLine = new List<string>();
-        //    foreach (var item in list)
-        //    {
-        //        Subtitle sub = new Subtitle();
-        //        string[] parts = item.Split(new string[] { Environment.NewLine },
-        //                       StringSplitOptions.RemoveEmptyEntries).ToArray();
-        //        sub.Id = Convert.ToInt32(parts[0]);
-        //        string[] times = parts[1].Split("-->", StringSplitOptions.RemoveEmptyEntries).ToArray();
-        //        sub.StartTime = TimeSpan.Parse(times[0].Replace(',', '.').Trim());
-        //        sub.EndTime = TimeSpan.Parse(times[1].Replace(',', '.').Trim());
-        //        string line = "";
-        //        for (int i = 2; i < parts.Length; i++)
-        //        {
-        //            line += parts[i];
-        //        }
-        //        sub.Text = line;
-        //        currentSub.Add(sub);
-        //    }
-        //    this.lstvdSub.ItemsSource = currentSub.Select(c => c.Text).ToArray();
-        //    this.lstvdSub.SelectedIndex = 0;
-        //}
-
-
-
-        //private void btnNext_Click(object sender, RoutedEventArgs e)
-        //{
-        //    try
-        //    {
-
-        //        string message = this.lstthSub.SelectedItem.ToString();
-        //        this.lstthSub.SelectedIndex = this.lstthSub.SelectedIndex + 1;
-        //        Task.Run(() =>
-        //        {
-        //            _SecondaryWindow.Dispatcher.BeginInvoke(new Action(() =>
-        //                    _SecondaryWindow.DisplayCurrentSub(this.currentSub[this.currentSubIndex].Text)));
-        //            SendGroupMessage(new ServiceMessage
-        //            {
-        //                Command = Command.DisplayManualSub,
-        //                Index = currentSubIndex,
-        //                Scene = this.currentScene
-        //            });
-        //        });
-        //        this.currentSubIndex++;
-        //    }
-        //    catch (Exception ex)
-        //    {
-
-        //    }
-        //}
-
-        //private void btnNextAuto_Click(object sender, RoutedEventArgs e)
-        //{
-        //    this.continueAutoLine = true;
-        //    //this.autoLine = new Task(() => AutoLine());
-        //    //this.autoLine.Start();
-
-        //    while (this.continueAutoLine)
-        //    {
-        //        string message = this.lstthSub.SelectedItem.ToString();
-        //        this.lstthSub.SelectedIndex = this.lstthSub.SelectedIndex + 1;
-        //        HUB.Clients.Group(ChatSettings.ChatGroup).SendAsync(ChatSettings.RecieveCommand, Swegrant.Shared.Models.ChatSettings.ServerUser, message);
-        //        int delay = message.Length * 70;
-        //        Thread.Sleep(delay);
-        //    }
-
-        //}
-
-        //private void AutoLine()
-        //{
-        //    while (this.continueAutoLine)
-        //    {
-        //        string message = this.lstthSub.SelectedItem.ToString();
-        //        this.lstthSub.SelectedIndex = this.lstthSub.SelectedIndex + 1;
-        //        HUB.Clients.Group(ChatSettings.ChatGroup).SendAsync(ChatSettings.RecieveCommand, Swegrant.Shared.Models.ChatSettings.ServerUser, message);
-        //        int delay = message.Length * 70;
-        //        Thread.Sleep(delay);
-        //    }
-        //}
-
-
-
-
-        //private void FillVideoListBox()
-        //{
-        //    string videoDirectory = $"{Directory.GetCurrentDirectory()}\\Video\\Background";
-        //    DirectoryInfo d = new DirectoryInfo(videoDirectory);
-        //    FileInfo[] Files = d.GetFiles();
-        //    //foreach( var file in  Files)
-        //    //{
-        //    //    this.lstthSub.Items.Add(file.Name);
-        //    //}
-        //    this.lstVideos.ItemsSource = Files.Select(c => c.Name).ToArray();
-        //}
-
-
-
-
-
-
-        //private void btnLoadSubTitle_Click(object sender, RoutedEventArgs e)
-        //{
-        //    try
-        //    {
-        //        MessageBoxResult result = MessageBox.Show("Are You Sure?", "Warning", MessageBoxButton.OKCancel);
-        //        if (result == MessageBoxResult.OK)
-        //        {
-        //            string text = "";
-        //            string lang = this.cmbthLanguage.SelectionBoxItem.ToString();
-        //            string scence = this.cmbthScence.SelectionBoxItem.ToString();
-        //            this.currentScene = Convert.ToInt32(scence);
-        //            string subtitleDirectory = $"{Directory.GetCurrentDirectory()}\\wwwroot\\MEDIA\\THSUB";
-        //            string subtitleFilePath = $"{subtitleDirectory}\\TH-SUB-{lang}-SC-{scence}.txt";
-        //            if (File.Exists(subtitleFilePath))
-        //            {
-        //                text = System.IO.File.ReadAllText(subtitleFilePath);
-        //                FillTHSbutitleListBox(text);
-        //                Task.Run(() =>
-        //                {
-        //                    SendGroupMessage(new ServiceMessage
-        //                    {
-        //                        Command = Command.Prepare,
-        //                        Mode = Mode.Theater,
-        //                        Scene = this.currentScene
-        //                    });
-        //                });
-        //            }
-        //            else
-        //            {
-        //                MessageBox.Show("File Does NOT Exist");
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message);
-        //    }
-        //}
-
-        //private void btnthPlayVideo_Click(object sender, RoutedEventArgs e)
-        //{
-        //    try
-        //    {
-        //        MessageBoxResult result = MessageBox.Show("Are You Sure?", "Warning", MessageBoxButton.OKCancel);
-        //        if (result == MessageBoxResult.OK)
-        //        {
-        //            string text = "";
-        //            //string lang = this.cmbthLanguage.SelectionBoxItem.ToString();
-        //            string scence = this.cmbthScence.SelectionBoxItem.ToString();
-        //            this.currentScene = Convert.ToInt32(scence);
-        //            string VideoDirectory = $"{Directory.GetCurrentDirectory()}\\Theater";
-        //            string videoFilePath = $"{VideoDirectory}\\TH-BK-SC-{scence}.mp4";
-        //            if (File.Exists(videoFilePath))
-        //            {
-        //                SendGroupMessage(new ServiceMessage
-        //                {
-        //                    Command = Command.Play,
-        //                    Mode = Mode.Theater,
-        //                    Scene = this.currentScene
-        //                });
-        //                this.currentSubCancelationSource = new CancellationTokenSource();
-        //                this.currentSubTask = Task.Run(() =>
-        //               {
-        //                   this.currentSubCancelationSource.Token.ThrowIfCancellationRequested();
-        //                   PlaySub();
-
-        //               }, this.currentSubCancelationSource.Token);
-        //                _SecondaryWindow.Play(videoFilePath);
-
-
-        //            }
-        //            else
-        //            {
-        //                MessageBox.Show("File Does NOT Exist");
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message);
-        //    }
-        //}
-
-        //private void btnvdLoadSubTitle_Click(object sender, RoutedEventArgs e)
-        //{
-        //    try
-        //    {
-        //        string text = "";
-        //        string charachter = this.cmbvdCharchter.SelectionBoxItem.ToString();
-        //        string lang = this.cmbvdLanguage.SelectionBoxItem.ToString();
-        //        string scence = this.cmbvdScence.SelectionBoxItem.ToString();
-        //        string subtitleDirectory = $"{Directory.GetCurrentDirectory()}\\wwwroot\\MEDIA\\VDSUB";
-        //        string subtitleFilePath = $"{subtitleDirectory}\\VD-{charachter}-SUB-{lang}-SC-{scence}.txt";
-        //        if (File.Exists(subtitleFilePath))
-        //        {
-        //            text = System.IO.File.ReadAllText(subtitleFilePath);
-        //            FillVideoSbutitleListBox(text);
-        //        }
-        //        else
-        //        {
-        //            MessageBox.Show("File Does NOT Exist");
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message);
-        //    }
-        //}
-
-        //private void btnvdPlayVideo_Click(object sender, RoutedEventArgs e)
-        //{
-        //    MessageBoxResult result = MessageBox.Show("Are You Sure?", "Warning", MessageBoxButton.OKCancel);
-        //    if (result == MessageBoxResult.OK)
-        //    {
-        //        string text = "";
-        //        //string lang = this.cmbthLanguage.SelectionBoxItem.ToString();
-        //        string scence = this.cmbvdScence.SelectionBoxItem.ToString();
-        //        this.currentScene = Convert.ToInt32(scence);
-        //        string VideoDirectory = $"{Directory.GetCurrentDirectory()}\\Video";
-        //        string videoFilePath = $"{VideoDirectory}\\VD-SC-{scence}.mp4";
-        //        if (File.Exists(videoFilePath))
-        //        {
-
-        //            SendGroupMessage(new ServiceMessage
-        //            {
-        //                Command = Command.Play,
-        //                Mode = Mode.Video,
-        //                Scene = this.currentScene
-        //            });
-
-        //            //this.currentSubCancelationSource = new CancellationTokenSource();
-        //            //this.currentSubTask = Task.Run(() =>
-        //            //{
-        //            //    this.currentSubCancelationSource.Token.ThrowIfCancellationRequested();
-        //            //    PlaySub();
-
-        //            //}, this.currentSubCancelationSource.Token);
-
-        //            _SecondaryWindow.Play(videoFilePath);
-
-        //            //PLayVideo(videoFilePath);
-        //            //Task.Run(PlaySub);
-        //            //_SecondaryWindow.Play(videoFilePath);
-
-
-        //        }
-        //        else
-        //        {
-        //            MessageBox.Show("File Does NOT Exist");
-        //        }
-        //    }
-        //}
-
-
-
-        //private void PlaySub()
-        //{
-        //    if (this.currentSubIndex == 0)
-        //    {
-        //        TimeSpan initial = this.currentSub[this.currentSubIndex].StartTime;
-        //        Thread.Sleep(initial);
-        //    }
-        //    for (int i = this.currentSubIndex; i < this.currentSub.Count - 1; i++)
-        //    {
-        //        try
-        //        {
-        //            if (this.currentSubCancelationSource.IsCancellationRequested)
-        //            {
-        //                this.currentSubCancellationToken.ThrowIfCancellationRequested();
-        //                return;
-        //            }
-
-        //            this.currentSubIndex = i;
-
-        //            //if (CurrentMode == Mode.Theater)
-        //            //{
-        //            //    _SecondaryWindow.Dispatcher.BeginInvoke(new Action(() =>
-        //            //       _SecondaryWindow.DisplayCurrentSub(currentSub[i].Text)
-        //            //        ));
-        //            //}
-        //            if (CurrentMode == Mode.Video)
-        //            {
-        //                txtCurrentLine.Dispatcher.BeginInvoke(new Action(() =>
-        //                {
-        //                    txtCurrentLine.Text = currentSub[i].Text;
-
-        //                }));
-        //            }
-
-        //            //HUB.Clients.Group(ChatSettings.ChatGroup).SendAsync(ChatSettings.RecieveCommand, ChatSettings.ServerUser, this.currentSub[i].Text);
-
-        //            Thread.Sleep(currentSub[i].Duration);
-        //            if (this.currentSubCancelationSource.IsCancellationRequested)
-        //            {
-        //                this.currentSubCancellationToken.ThrowIfCancellationRequested();
-        //                return;
-        //            }
-
-        //            //if (CurrentMode == Mode.Theater)
-        //            //{
-        //            //    _SecondaryWindow.Dispatcher.BeginInvoke(new Action(() =>
-        //            //        _SecondaryWindow.DisplayCurrentSub(" ")
-        //            //        ));
-        //            //    this.Dispatcher.BeginInvoke(new Action(() =>
-        //            //       this.lstthSub.SelectedIndex = this.lstthSub.SelectedIndex + 1
-        //            //    ));
-        //            //}
-        //            if (CurrentMode == Mode.Video)
-        //            {
-        //                txtCurrentLine.Dispatcher.BeginInvoke(new Action(() =>
-        //                {
-        //                    txtCurrentLine.Text = "";
-        //                }));
-        //                this.Dispatcher.BeginInvoke(new Action(() =>
-        //                   this.lstvdSub.SelectedIndex = this.lstvdSub.SelectedIndex + 1
-        //                ));
-        //            }
-
-        //            //HUB.Clients.Group(ChatSettings.ChatGroup).SendAsync(ChatSettings.RecieveCommand, ChatSettings.ServerUser, " ");
-
-        //            TimeSpan gap = currentSub[i + 1].StartTime - currentSub[i].EndTime;
-        //            Thread.Sleep(gap);
-        //        }
-        //        catch (Exception ex)
-        //        {
-
-        //        }
-        //    }
-        //}
-
-
-
-        //private void TabControl_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        //{
-        //    if (e.Source is TabControl)
-        //    {
-
-        //        if (this.currentSubTask != null && this.currentSubTask.Status == TaskStatus.Running)
-        //        {
-        //            this.currentSubCancelationSource.Cancel();
-        //        }
-        //        this.currentSubIndex = 0;
-
-
-        //        TabControl tab = (TabControl)e.Source;
-        //        this.CurrentMode = (tab.SelectedIndex == 0 ? Mode.Theater : Mode.Video);
-        //        SendGroupMessage(new ServiceMessage
-        //        {
-        //            Command = Command.ChangeMode,
-        //            Mode = this.CurrentMode,
-        //        });
-        //        //do work when tab is changed
-        //    }
-        //}
 
 
     }
